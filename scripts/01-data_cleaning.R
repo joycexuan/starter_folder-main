d804e4bfcc014e5f38dd5dab48be4fbd1465ad9a
Joyce Xuan <joyceruoxuan@gmail.com>
2023-02-02 13:15:55 -0500

    changes

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(janitor)
 library(knitr)
 
+assaults <- read.csv("~/Desktop/starter_folder-main/inputs/data/cleaned_assault_data.csv")
+
 ##selecting neighbourhood name, 2020 assault amounts and 2021 assault amounts
 assaults <- assault_data |> select(HoodName, Assault_2020, Assault_2021)
 ##selecting assaults that are above 400 from both 2020 and 2021
@@ -14,12 +16,19 @@ filter_assaults <- assaults |> pivot_longer(cols=c("Assault_2020", "Assault_2021
                                             names_to = "assault_year", 
                                             values_to = "assault_amt")
 
+##renaming columns so they are more elegant
+filter_assaults <- filter_assaults |>
+  rename(
+    Neighbourhood = HoodName,
+    Assault_Number = assault_amt,
+    Assault_Year = assault_year
+  )
+
 ##renaming a neighbourhood so its shorter
 filter_assaults <- filter_assaults |> 
   mutate(
     Neighbourhood =
-      recode(
-        Neighbourhood,
+      recode(Neighbourhood,
         "Waterfront Communities-The Island" = "Waterfront-Island"
       )
   )
@@ -35,14 +44,12 @@ filter_assaults <- filter_assaults |>
       )
   )
 
-##renaming columns so they are more elegant
-filter_assaults <- filter_assaults |>
-  rename(
-    Neighbourhood = HoodName,
-    Assault_Number = assault_amt,
-    Assault_Year = assault_year
-  )
 
 ##making a bar graph with everything
-ggplot(filter_assaults, aes(x = Neighbourhood, y=Assault_Number, fill=Assault_Year)) + geom_bar (stat="identity", position="dodge")+ theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.8))
+ggplot(filter_assaults, aes(x = Neighbourhood, 
+                            y=Assault_Number, fill=Assault_Year)) + 
+  geom_bar (stat="identity", position="dodge")+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.8))
+
 write.csv(filter_assaults, "~/Desktop/starter_folder-main/inputs/data/cleaned_assault_data.csv", row.names=FALSE)
+
